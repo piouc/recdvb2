13fd9bdccda1298274208462c89b109f9328ad79
piou <piou@hotmail.co.jp>
2019-10-04 00:26:03 +0900

    patch https://gist.github.com/2GMon/3621dd5054ab20c2a8c565fc236de093

--- recpt1.c
+++ recpt1.c
@@ -51,23 +51,23 @@ struct {
 	char *channel;
 	int tsid;
 } preset_ch[NUM_PRESET_CH] = {
-	{ 101, "bs15", 0x40f1 }, { 103, "bs03", 0x4031 },
-	{ 141, "bs13", 0x40d0 }, { 151, "bs01", 0x4010 },
-	{ 161, "bs01", 0x4011 }, { 171, "bs01", 0x4012 },
-	{ 181, "bs13", 0x40d1 }, { 191, "bs03", 0x4030 },
-	{ 192, "bs05", 0x4450 }, { 193, "bs05", 0x4451 },
-	{ 200, "bs09", 0x4091 }, { 201, "bs15", 0x40f2 },
-	{ 202, "bs15", 0x40f2 }, { 211, "bs09", 0x4490 },
-	{ 222, "bs09", 0x4092 }, { 231, "bs11", 0x46b2 },
-	{ 232, "bs11", 0x46b2 }, { 233, "bs11", 0x46b2 },
-	{ 234, "bs19", 0x4730 }, { 236, "bs13", 0x46d2 },
-	{ 238, "bs11", 0x46b0 }, { 241, "bs11", 0x46b1 },
-	{ 242, "bs19", 0x4731 }, { 243, "bs19", 0x4732 },
-	{ 244, "bs21", 0x4751 }, { 245, "bs21", 0x4752 },
-	{ 251, "bs23", 0x4770 }, { 252, "bs21", 0x4750 },
-	{ 255, "bs23", 0x4771 }, { 256, "bs03", 0x4632 },
-	{ 258, "bs23", 0x4772 }, { 531, "bs11", 0x46b2 },
-	{ 910, "bs15", 0x40f2 }, { 929, "bs15", 0x40f1 },
+	{ 101, "bs15_0", 0x40f1 }, { 103, "bs03_1", 0x4031 },
+	{ 141, "bs13_0", 0x40d0 }, { 151, "bs01_0", 0x4010 },
+	{ 161, "bs01_1", 0x4011 }, { 171, "bs01_2", 0x4012 },
+	{ 181, "bs13_1", 0x40d1 }, { 191, "bs03_0", 0x4030 },
+	{ 192, "bs05_0", 0x4450 }, { 193, "bs05_1", 0x4451 },
+	{ 200, "bs09_1", 0x4091 }, { 201, "bs15_2", 0x40f2 },
+	{ 202, "bs15_2", 0x40f2 }, { 211, "bs09_0", 0x4090 },
+	{ 222, "bs09_2", 0x4092 }, { 231, "bs11_2", 0x46b2 },
+	{ 232, "bs11_2", 0x46b2 }, { 233, "bs11_2", 0x46b2 },
+	{ 234, "bs19_0", 0x4730 }, { 236, "bs13_2", 0x46d2 },
+	{ 238, "bs11_0", 0x46b0 }, { 241, "bs11_1", 0x46b1 },
+	{ 242, "bs19_1", 0x4731 }, { 243, "bs19_2", 0x4732 },
+	{ 244, "bs21_1", 0x4751 }, { 245, "bs21_2", 0x4752 },
+	{ 251, "bs23_0", 0x4770 }, { 252, "bs21_0", 0x4750 },
+	{ 255, "bs23_1", 0x4771 }, { 256, "bs03_2", 0x4632 },
+	{ 258, "bs23_2", 0x4772 }, { 531, "bs11_0", 0x46b2 },
+	{ 910, "bs15_0", 0x40f2 }, { 929, "bs15_0", 0x40f1 },
 	{ 296, "nd02", 0x6020 }, { 298, "nd02", 0x6020 },
 	{ 299, "nd02", 0x6020 }, { 100, "nd04", 0x7040 },
 	{ 223, "nd04", 0x7040 }, { 227, "nd04", 0x7040 },
@@ -107,6 +107,33 @@ void set_lch(char *lch, char **ppch, char **sid, unsigned int *tsid)
 		*ppch = preset_ch[i].channel;
 		if (*tsid == 0) *tsid = preset_ch[i].tsid;
 		if (*sid == NULL) *sid = lch;
+	}
+}
+
+void set_bs_tsid(char *pch, unsigned int *tsid)
+{
+	int i;
+	char channel[8];
+	if (((pch[0] == 'b')|| (pch[0] == 'B')) && ((pch[1] == 's') || (pch[1] == 'S'))) {
+		channel[0] = 'b';
+		channel[1] = 's';
+	} else {
+		*tsid = 0;
+		return;
+	}
+	for (i = 2; pch[i] != '\0'; i++) {
+		channel[i] = pch[i];
+	}
+	pch[i] = '\0';
+	
+	for (i = 0; i < NUM_PRESET_CH; i++)
+		if (strcmp(preset_ch[i].channel, channel) == 0) break;
+	
+	if (i == NUM_PRESET_CH) {
+		*tsid = 0;
+	} else {
+		*tsid = preset_ch[i].tsid;
+		fprintf(stderr, "tsid = 0x%04x\n", *tsid);
 	}
 }
 
@@ -868,6 +895,7 @@ if(use_http){	// http-server add-
         fprintf(stderr, "tsid = 0x%x\n", tsid);
     }
     if(pch == NULL) pch = argv[optind];
+    if(!tsid) set_bs_tsid(pch, &tsid);
 
     /* tune */
     if(tune(pch, &tdata, dev_num, tsid) != 0)
